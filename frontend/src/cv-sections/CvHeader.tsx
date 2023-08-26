3366226b8b6188e1436010bb487a5dfb7fc20805
levg34 <levg34@users.noreply.github.com>
2023-08-26 21:03:04 +0000

    try skeleton (does not seem to work)

--- frontend/src/cv-sections/CvHeader.tsx
+++ frontend/src/cv-sections/CvHeader.tsx
@@ -1,16 +1,19 @@
-import { Avatar, Card } from "@suid/material";
+import { Avatar, Card, Skeleton } from "@suid/material";
 import { Header } from "../types";
 import { For } from "solid-js";
 
 export default (section: Header) => <Card>
+    <Skeleton variant="circular" animation='wave'>
+        <Avatar />
+    </Skeleton>
     <Avatar
-        alt="John Doe"
+        alt={`${section.infos.name}`}
         src={section.infos.pictureUrl}
     />
     <p>{section.position.join(', ')}</p>
     <ul>
-    <For each={Object.keys(section.infos)}>
-        {(key) => <li>{key}: {section.infos[key]}</li>} 
-    </For>
+        <For each={Object.keys(section.infos)}>
+            {(key) => <li>{key}: {section.infos[key]}</li>}
+        </For>
     </ul>
 </Card>
