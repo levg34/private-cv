e1e1eb621a2aa822c0bd8dd3fda349c5a24c49f5
levg34 <levg34@users.noreply.github.com>
2023-08-31 19:44:12 +0000

    setup backend to send cv and frontend to do http request

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,16 @@
-import Koa from 'koa';
-const app = new Koa();
+import Koa from 'koa'
+const app = new Koa()
+
+import cv from './data/cv.json'
 
 app.use(async ctx => {
-  ctx.body = 'Setup Koa';
-});
+    if (ctx.method === 'GET' && ctx.path === '/cv') {
+        ctx.set('Access-Control-Allow-Origin', '*')
+        ctx.set('Content-Type', 'application/json')
+        ctx.body = cv
+    } else {
+        ctx.body = '404'
+    }
+})
 
-app.listen(3000);
+app.listen(3000)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import Header from './Header'
 import CV from './CV'
-import cv from '../../cv.json'
+import { Match, Switch, createResource } from 'solid-js'
+import axios from 'axios'
+import { Alert, CircularProgress } from '@suid/material'
 
 export default function App() {
+    const [cv] = createResource(() => axios.get('/cv.json'))
+
     return (
         <>
             <Header />
-            <CV {...cv} />
+            <Switch fallback={<CV {...cv().data} />}>
+                <Match when={cv.loading}><CircularProgress color="secondary" /> Loading...</Match>
+                <Match when={cv.error}><Alert severity="error">Error: {cv.error}</Alert></Match>
+            </Switch>
         </>
     )
 }
