151ebcf34447a45f96718eca18c74f3cb7e13841
levg34 <levg34@users.noreply.github.com>
2023-09-26 18:23:56 +0000

    remove warnings in tests

--- frontend/test/CartProvider.test.tsx
+++ frontend/test/CartProvider.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen } from "@solidjs/testing-library"
 import CartProvider, { useCart } from "../src/CartProvider"
+import { createRoot } from 'solid-js'
 
 const CartChild = () => {
     const { clearCart, toggleCart, addToCart, removeFromCart, validateCart, getCartItems, getStatus } = useCart()
@@ -20,124 +21,148 @@ const CartChild = () => {
 }
 
 describe("CartProvider", () => {
-    it("should render the cart status and required info", () => {
-        const compo = render(() =>
-            <CartProvider>
-                <CartChild />
-            </CartProvider>
-        )
+    it("should render the cart status and required info", async () => {
+        await createRoot(async () => {
+            const compo = render(() =>
+                <CartProvider>
+                    <CartChild />
+                </CartProvider>
+            )
 
-        // expect the initial status to be empty
-        expect(screen.getByText("Status: empty")).toBeInTheDocument()
+            await Promise.resolve()
 
-        // expect the initial required info to be empty
-        expect(compo.getByText("Required info:")).toBeInTheDocument()
+            // expect the initial status to be empty
+            expect(screen.getByText("Status: empty")).toBeInTheDocument()
+
+            // expect the initial required info to be empty
+            expect(compo.getByText("Required info:")).toBeInTheDocument()
+        })
     })
 
-    it("should toggle an item in the cart", () => {
-        const compo = render(() =>
-            <CartProvider>
-                <CartChild />
-            </CartProvider>
-        )
+    it("should toggle an item in the cart", async () => {
+        await createRoot(async () => {
+            const compo = render(() =>
+                <CartProvider>
+                    <CartChild />
+                </CartProvider>
+            )
 
-        // click on the toggle item1 button
-        compo.getByText("Toggle item1").click()
+            await Promise.resolve()
 
-        // expect the status to be dirty
-        expect(compo.getByText("Status: dirty")).toBeInTheDocument()
+            // click on the toggle item1 button
+            compo.getByText("Toggle item1").click()
 
-        // expect the required info to contain item1
-        expect(compo.getByText("Required info: item1")).toBeInTheDocument()
+            // expect the status to be dirty
+            expect(compo.getByText("Status: dirty")).toBeInTheDocument()
 
-        // click on the toggle item1 button again
-        compo.getByText("Toggle item1").click()
+            // expect the required info to contain item1
+            expect(compo.getByText("Required info: item1")).toBeInTheDocument()
 
-        // expect the status to be dirty
-        expect(compo.getByText("Status: dirty")).toBeInTheDocument()
+            // click on the toggle item1 button again
+            compo.getByText("Toggle item1").click()
 
-        // expect the required info to be empty
-        expect(compo.getByText("Required info:")).toBeInTheDocument()
+            // expect the status to be dirty
+            expect(compo.getByText("Status: dirty")).toBeInTheDocument()
+
+            // expect the required info to be empty
+            expect(compo.getByText("Required info:")).toBeInTheDocument()
+        })
     })
 
-    it("should add an item to the cart", () => {
-        render(() =>
-            <CartProvider>
-                <CartChild />
-            </CartProvider>
-        );
+    it("should add an item to the cart", async () => {
+        await createRoot(async () => {
+            render(() =>
+                <CartProvider>
+                    <CartChild />
+                </CartProvider>
+            );
 
-        // click on the add item2 button
-        screen.getByText("Add item2").click();
-
-        // expect the status to be dirty
-        expect(screen.getByText("Status: dirty")).toBeInTheDocument();
-
-        // expect the required info to contain item2
-        expect(screen.getByText("Required info: item2")).toBeInTheDocument();
+            await Promise.resolve()
+    
+            // click on the add item2 button
+            screen.getByText("Add item2").click();
+    
+            // expect the status to be dirty
+            expect(screen.getByText("Status: dirty")).toBeInTheDocument();
+    
+            // expect the required info to contain item2
+            expect(screen.getByText("Required info: item2")).toBeInTheDocument();
+        })
     });
 
-    it("should remove an item from the cart", () => {
-        render(() =>
-            <CartProvider>
-                <CartChild />
-            </CartProvider>
-        );
+    it("should remove an item from the cart", async () => {
+        await createRoot(async () => {
+            render(() =>
+                <CartProvider>
+                    <CartChild />
+                </CartProvider>
+            );
 
-        // add item2 to the cart
-        screen.getByText("Add item2").click();
-
-        // click on the remove item2 button
-        screen.getByText("Remove item2").click();
-
-        // expect the status to be dirty
-        expect(screen.getByText("Status: dirty")).toBeInTheDocument();
-
-        // expect the required info to be empty
-        expect(screen.getByText("Required info:")).toBeInTheDocument();
+            await Promise.resolve()
+    
+            // add item2 to the cart
+            screen.getByText("Add item2").click();
+    
+            // click on the remove item2 button
+            screen.getByText("Remove item2").click();
+    
+            // expect the status to be dirty
+            expect(screen.getByText("Status: dirty")).toBeInTheDocument();
+    
+            // expect the required info to be empty
+            expect(screen.getByText("Required info:")).toBeInTheDocument();
+        })
     });
 
-    it("should validate the cart", () => {
-        render(() =>
-            <CartProvider>
-                <CartChild />
-            </CartProvider>
-        );
+    it("should validate the cart", async () => {
+        await createRoot(async () => {
+            render(() =>
+                <CartProvider>
+                    <CartChild />
+                </CartProvider>
+            );
 
-        // add item1 and item2 to the cart
-        screen.getByText("Add item1").click();
-        screen.getByText("Add item2").click();
-
-        // click on the validate cart button
-        screen.getByText("Validate cart").click();
-
-        // expect the status to be validated
-        expect(screen.getByText("Status: validated")).toBeInTheDocument();
-
-        // expect the required info to contain item1 and item2
-        expect(screen.getByText("Required info: item1, item2")).toBeInTheDocument();
+            await Promise.resolve()
+    
+            // add item1 and item2 to the cart
+            screen.getByText("Add item1").click();
+            screen.getByText("Add item2").click();
+    
+            // click on the validate cart button
+            screen.getByText("Validate cart").click();
+    
+            // expect the status to be validated
+            expect(screen.getByText("Status: validated")).toBeInTheDocument();
+    
+            // expect the required info to contain item1 and item2
+            expect(screen.getByText("Required info: item1, item2")).toBeInTheDocument();
+        })
     });
 
-    it("should clear the cart", () => {
-        render(() =>
-            <CartProvider>
-                <CartChild />
-            </CartProvider>
-        );
+    it("should clear the cart", async () => {
+        await createRoot(async () => {
+            render(() =>
+                <CartProvider>
+                    <CartChild />
+                </CartProvider>
+            );
 
-        // add item1 and item2 to the cart
-        screen.getByText("Add item1").click();
-        screen.getByText("Add item2").click();
-
-        // click on the clear cart button
-        screen.getByText("Clear cart").click();
-
-        // expect the status to be empty
-        // expect(screen.getByText("Status: empty")).toBeInTheDocument();
-        expect(screen.getByText("Status: dirty")).toBeInTheDocument();
-
-        // expect the required info to be empty
-        expect(screen.getByText("Required info:")).toBeInTheDocument();
+            await Promise.resolve()
+    
+            // add item1 and item2 to the cart
+            screen.getByText("Add item1").click();
+            screen.getByText("Add item2").click();
+    
+            // click on the clear cart button
+            screen.getByText("Clear cart").click();
+    
+            // expect the status to be empty
+            // expect(screen.getByText("Status: empty")).toBeInTheDocument();
+            expect(screen.getByText("Status: dirty")).toBeInTheDocument();
+    
+            // expect the required info to be empty
+            expect(screen.getByText("Required info:")).toBeInTheDocument();
+        })
     });
 
 })
