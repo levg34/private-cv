e277c82541dae304fbfe9d4c86e1531a3f392adb
levg34 <levg34@users.noreply.github.com>
2023-10-27 23:37:28 +0200

    Update utils.ts
    new algorithm for hiding data

--- backend/utils/utils.ts
+++ backend/utils/utils.ts
@@ -62,3 +62,33 @@ const maskData = (data: any, mask: any): any => {
     // Si le masque n’est ni un booléen, ni un tableau, ni un objet, on retourne la donnée
     return data
 }
+
+function remove(obj: any, mask: any): any {
+    if (typeof mask === 'object') {
+        if (Array.isArray(mask) && Array.isArray(obj)) {
+            mask.forEach((maskElement, index) => {
+                if (maskElement === true) {
+                    obj[index] = null;
+                } else if (typeof maskElement === 'object' && obj[index] !== undefined) {
+                    obj[index] = remove(obj[index], maskElement);
+                }
+            });
+        } else {
+            for (const key in mask) {
+                if (mask[key] === true) {
+                    obj[key] = null;
+                } else if (typeof mask[key] === 'object' && obj[key] !== undefined) {
+                    if (Array.isArray(obj[key]) && !Array.isArray(mask[key])) {
+                        for (let i = 0; i < obj[key].length; i++) {
+                            obj[key][i] = remove(obj[key][i], mask[key]);
+                        }
+                    } else {
+                        obj[key] = remove(obj[key], mask[key]);
+                    }
+                }
+            }
+        }
+    }
+    return obj;
+}
+
