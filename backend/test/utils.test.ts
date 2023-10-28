1f6f0ff84ffb47fdedce2ef259b05e1927803e03
levg34 <levg34@users.noreply.github.com>
2023-10-28 10:28:56 +0000

    add remove function and tests

--- backend/test/utils.test.ts
+++ backend/test/utils.test.ts
@@ -1,4 +1,4 @@
-import { hideData } from '../utils/utils'
+import { hideData, remove } from '../utils/utils'
 import cv from './data/test-cv.json'
 import maskedCv from './data/test-hidden.json'
 import maskedCv2 from './data/test-hidden-2.json'
@@ -45,5 +45,119 @@ describe('test utils', () => {
         }
 
         expect(hideData(cv, mask as unknown as Mask)).toEqual(maskedCv2)
+    })
+})
+
+describe('remove function', () => {
+    it('should remove fields in an object based on the mask', () => {
+        const obj = {
+            name: 'John',
+            age: 30,
+            jobHistory: [
+                { city: 'New York', year: 2020 },
+                { city: 'Los Angeles', year: 2019 }
+            ]
+        }
+        const mask = { name: true, age: true }
+        const result = remove(obj, mask)
+        expect(result).toEqual({ name: null, age: null, jobHistory: obj.jobHistory })
+    })
+
+    it('should remove fields in an array of objects based on the mask', () => {
+        const obj = {
+            name: 'John',
+            jobHistory: [
+                { city: 'New York', year: 2020 },
+                { city: 'Los Angeles', year: 2019 }
+            ]
+        }
+        const mask = { jobHistory: { city: true } }
+        const result = remove(obj, mask)
+        expect(result).toEqual({
+            name: obj.name,
+            jobHistory: [
+                { city: null, year: 2020 },
+                { city: null, year: 2019 }
+            ]
+        })
+    })
+
+    it('should handle nested objects and arrays in the mask', () => {
+        const obj = {
+            name: 'John',
+            jobHistory: [
+                { city: 'New York', year: 2020 },
+                { city: 'Los Angeles', year: 2019 }
+            ]
+        }
+        const mask = { jobHistory: [false, { city: true }] }
+        const result = remove(obj, mask)
+        expect(result).toEqual({
+            name: obj.name,
+            jobHistory: [
+                { city: 'New York', year: 2020 },
+                { city: null, year: 2019 }
+            ]
+        })
+    })
+
+    it('should handle complex masks', () => {
+        const obj = {
+            name: 'John',
+            jobHistory: [
+                { city: 'New York', year: 2020 },
+                { city: 'Los Angeles', year: 2019 }
+            ],
+            education: {
+                school: 'University',
+                degree: 'Bachelor'
+            }
+        }
+        const mask = {
+            jobHistory: { city: true },
+            education: { school: true, degree: true }
+        }
+        const result = remove(obj, mask)
+        expect(result).toEqual({
+            name: 'John',
+            jobHistory: [
+                { city: null, year: 2020 },
+                { city: null, year: 2019 }
+            ],
+            education: {
+                school: null,
+                degree: null
+            }
+        })
+    })
+
+    xit('should handle complex high level masks', () => {
+        const obj = {
+            name: 'John',
+            jobHistory: [
+                { city: 'New York', year: 2020 },
+                { city: 'Los Angeles', year: 2019 }
+            ],
+            education: {
+                school: 'University',
+                degree: 'Bachelor'
+            }
+        }
+        const mask = {
+            jobHistory: { city: true },
+            education: true
+        }
+        const result = remove(obj, mask)
+        expect(result).toEqual({
+            name: 'John',
+            jobHistory: [
+                { city: null, year: 2020 },
+                { city: null, year: 2019 }
+            ],
+            education: {
+                school: null,
+                degree: null
+            }
+        })
     })
 })
--- backend/utils/utils.ts
+++ backend/utils/utils.ts
@@ -2,93 +2,36 @@ import { Mask, MaskedCV } from '../types/mask-types'
 import { CurriculumVitae } from '../types/types'
 
 export const hideData = (cv: CurriculumVitae, mask: Mask): MaskedCV => {
-    // On crée une copie du cv pour ne pas modifier l’original
     const maskedCv = JSON.parse(JSON.stringify(cv))
-    // On parcourt les clés du masque
-    for (const key of Object.keys(mask)) {
-        // On vérifie si la clé existe dans le cv
-        if (key in maskedCv) {
-            // On appelle une fonction récursive qui va masquer les données selon le masque
-            maskedCv[key] = maskData(maskedCv[key], (mask as Record<string, unknown>)[key])
-        }
-    }
-    // On retourne le cv masqué
-    return maskedCv
+    return remove(maskedCv, mask)
 }
 
-// Fonction récursive qui prend en paramètre une donnée et un masque
-const maskData = (data: any, mask: any): any => {
-    // Si le masque est un booléen, on retourne null si il est vrai, sinon on retourne la donnée
-    if (typeof mask === 'boolean') {
-        return mask ? null : data
-    }
-    // Si le masque est un tableau, on vérifie que la donnée aussi
-    if (Array.isArray(mask)) {
-        if (Array.isArray(data)) {
-            // On crée un nouveau tableau vide
-            const maskedArray = []
-            // On parcourt les éléments du masque et de la donnée
-            for (let i = 0; i < mask.length; i++) {
-                // On appelle récursivement la fonction sur chaque élément
-                maskedArray[i] = maskData(data[i], mask[i])
-            }
-            // On retourne le tableau masqué
-            return maskedArray
-        } else {
-            // Si la donnée n’est pas un tableau, on retourne null
-            return null
-        }
-    }
-    // Si le masque est un objet, on vérifie que la donnée aussi
-    if (typeof mask === 'object') {
-        if (typeof data === 'object') {
-            // On crée un nouvel objet vide
-            const maskedObject: Record<string, any> = {}
-            // On parcourt les clés du masque et de la donnée
-            for (const key of Object.keys(mask)) {
-                // On vérifie si la clé existe dans la donnée
-                if (key in data) {
-                    // On appelle récursivement la fonction sur chaque propriété
-                    maskedObject[key] = maskData(data[key], mask[key])
-                }
-            }
-            // On retourne l’objet masqué
-            return maskedObject
-        } else {
-            // Si la donnée n’est pas un objet, on retourne null
-            return null
-        }
-    }
-    // Si le masque n’est ni un booléen, ni un tableau, ni un objet, on retourne la donnée
-    return data
-}
-
-function remove(obj: any, mask: any): any {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function remove(obj: any, mask: any): any {
     if (typeof mask === 'object') {
         if (Array.isArray(mask) && Array.isArray(obj)) {
             mask.forEach((maskElement, index) => {
                 if (maskElement === true) {
-                    obj[index] = null;
+                    obj[index] = null
                 } else if (typeof maskElement === 'object' && obj[index] !== undefined) {
-                    obj[index] = remove(obj[index], maskElement);
+                    obj[index] = remove(obj[index], maskElement)
                 }
-            });
+            })
         } else {
             for (const key in mask) {
                 if (mask[key] === true) {
-                    obj[key] = null;
+                    obj[key] = null
                 } else if (typeof mask[key] === 'object' && obj[key] !== undefined) {
                     if (Array.isArray(obj[key]) && !Array.isArray(mask[key])) {
                         for (let i = 0; i < obj[key].length; i++) {
-                            obj[key][i] = remove(obj[key][i], mask[key]);
+                            obj[key][i] = remove(obj[key][i], mask[key])
                         }
                     } else {
-                        obj[key] = remove(obj[key], mask[key]);
+                        obj[key] = remove(obj[key], mask[key])
                     }
                 }
             }
         }
     }
-    return obj;
+    return obj
 }
-
