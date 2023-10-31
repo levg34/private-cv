3ef38398d1de4a7a71bab5622e248a8a1c5beda4
levg34 <levg34@users.noreply.github.com>
2023-10-31 20:36:09 +0000

    fix #3: Make linting mandatory

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -7,7 +7,7 @@ import { useCart } from './CartProvider'
 export default () => {
     const [loggedIn, setLoggedIn] = createSignal<boolean>(true)
     const [showMenu, setShowMenu] = createSignal<boolean>(false)
-    let anchorEl: Element | SVGSVGElement | ((element: Element) => Element) | ((el: SVGSVGElement) => void)
+    let anchorEl
     const handleClose = () => {
         setShowMenu(false)
     }
